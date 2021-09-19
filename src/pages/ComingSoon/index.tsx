dfe42250e628e4ac121c2714cd75b3acb8d8bb34
Matheww <basemagalha@gmail.com>
2021-09-19 18:48:32 -0300

    Add archives

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,17 @@
+import  React from 'react';
+
+import { Text } from 'react-native';
+
+import { Wrapper, Heading } from './styles';
+
+
+
+const ComingSoon:React.FC = () => {
+  return (
+    <Wrapper>
+      <Heading>Coming Soon....</Heading>
+    </Wrapper>
+  );
+};
+
+export default ComingSoon;
---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,17 @@
+import  React from 'react';
+
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+
+
+const Following: React.FC = () => {
+  return (
+    <Container>
+      <Text>Following</Text>
+    </Container>
+  );
+};
+
+export default Following;
